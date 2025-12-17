7d11e11375381595db9a5dab3466ac8c25f3677b
2204030037-sketch <2204030037@students.unis.ac.id>
2025-12-17 15:30:19 +0700

    Update music

--- script.js
+++ script.js
@@ -10,3 +10,49 @@ const nama = params.get("to");
 if (nama) {
   document.getElementById("tujuan").innerText = nama;
 }
+
+const music = document.getElementById("bgMusic");
+let isPlaying = false;
+
+function buka() {
+  document.getElementById("cover").style.display = "none";
+  document.getElementById("surat").style.display = "block";
+
+  music.play();
+  isPlaying = true;
+}
+
+function toggleMusic() {
+  if (isPlaying) {
+    music.pause();
+    isPlaying = false;
+  } else {
+    music.play();
+    isPlaying = true;
+  }
+}
+
+music.volume = 0;
+music.play();
+let vol = 0;
+let fade = setInterval(() => {
+  if (vol < 1) {
+    vol += 0.05;
+    music.volume = vol;
+  } else {
+    clearInterval(fade);
+  }
+}, 200);
+
+function toggleMusic() {
+  const btn = document.querySelector(".music-btn");
+  if (isPlaying) {
+    music.pause();
+    isPlaying = false;
+    btn.textContent = "🔇";
+  } else {
+    music.play();
+    isPlaying = true;
+    btn.textContent = "🔊";
+  }
+}
